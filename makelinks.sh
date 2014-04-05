f5de34c3c8a12f38932e72ed550e593cf8ac4377
andytuba <andytuba@andytuba.com>
2014-04-05 10:13:11 -0500

    include HTMLPasteurizer in makelinks build script

--- makelinks.sh
+++ makelinks.sh
@@ -1,7 +1,7 @@
 files=("reddit_enhancement_suite.user.js" "res.css" "nightmode.css" "commentBoxes.css" \
 	"jquery-1.9.1.min.js" "jquery-fieldselection.min.js" \
 	"jquery.dragsort-0.5.1.js" "jquery.tokeninput.js" \
-	"tinycon.js" "snuownd.js" "guiders-1.2.8.js")
+	"tinycon.js" "snuownd.js" "guiders-1.2.8.js", "HTMLPasteurizer.js")
 paths=("Chrome" "XPI/data" "Opera" "OperaBlink" "RES.safariextension")
 
 for i in "${files[@]}"
